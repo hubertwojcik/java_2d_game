423504aa4db24a95badf98d4ef7dccce10e0ebd3
Hubert Wójcik <hwojcikk@gmail.com>
2024-12-20 22:34:48 +0100

    add map

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,7 +4,10 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gp;
@@ -18,9 +21,8 @@ public class TileManager {
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
+        loadMap("/maps/map");
     }
-
-
 
     public void getTileImage(){
         try{
@@ -37,6 +39,37 @@ public class TileManager {
         }
     }
 
+    public void loadMap(String filePath){
+        try{
+            InputStream is = getClass().getResourceAsStream(filePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while(col < gp.maxScreenCol && row < gp.maxScreenRow){
+                String line = br.readLine();
+                System.out.println(line);
+                while(col < gp.maxScreenCol){
+                    String numbers[] = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if(col == gp.maxScreenCol){
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
     public void draw(Graphics2D g2){
 
         int col  = 0;
@@ -45,7 +78,8 @@ public class TileManager {
         int y =0;
 
         while(col < gp.maxScreenCol && row < gp.maxScreenRow){
-            g2.drawImage(tile[0].image,x,y,gp.tileSize,gp.tileSize,null);
+            int tileNum = mapTileNum[col][row];
+            g2.drawImage(tile[tileNum].image,x,y,gp.tileSize,gp.tileSize,null);
             col++;
             x+=gp.tileSize;
 
